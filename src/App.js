058b210943e9d06060ca34914126995259fbcf1e
ptthao0801 <ptthao0801@gmail.com>
2024-07-02 16:51:26 +0700

    first cm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            color: 'black'
+        };
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+componentDidMount() {
+    setTimeout(() => {
+        this.setState({ color: 'pink' });
+    }, 1000);
 }
+
+render() {
+    return (
+        <div>
+            <div
+                style={{
+                    backgroundColor: this.state.color,
+                    paddingTop: 20,
+                    width: 400,
+                    height: 80,
+                    margin: 'auto'
+                }}
+            />
+        </div>
+    );
+}
+}
+
 
 export default App;
